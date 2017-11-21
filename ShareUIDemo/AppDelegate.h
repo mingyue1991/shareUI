ffc447c55ead7df4fab70d5de39e253cdc5f3e5c
mingyue3 <mingyue3@staff.weibo.com>
2017-11-21 09:49:29 +0800

    Initial Commit

---
+++ ShareUIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareUIDemo
+//
+//  Created by swit on 2017/11/21.
+//  Copyright © 2017年 swit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
